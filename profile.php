f0ddc76658685395b07686a6e8f407874670c530
Netmosfera <netmosfera@gmail.com>
2018-09-22 00:52:42 +0200

    Use offset in preg_match

--- profile.php
+++ profile.php
@@ -6,13 +6,9 @@ require(__DIR__ . "/vendor/autoload.php");
 
 $css = file_get_contents(__DIR__ . "/1.css");
 
-echo "jit: " . ini_get("pcre.jit") . "\n";
+gc_disable();
 
-$tokenizer = new StandardTokenizer();
-$s = microtime(TRUE);
-$result = $tokenizer->tokenize($css);
-echo number_format(microtime(TRUE) - $s, 10) . "\n";
-file_put_contents(__DIR__ . "/tokens.txt", print_r($result, TRUE));
+echo "jit: " . ini_get("pcre.jit") . "\n";
 
 $tokenizer = new StandardTokenizer(FALSE);
 $s = microtime(TRUE);
--- src/Traverser.php
+++ src/Traverser.php
@@ -6,28 +6,37 @@ namespace Netmosfera\PHPCSSAST;
 
 use Error;
 use function mb_substr;
-use function preg_quote;
 use function preg_last_error;
+use function preg_quote;
 
 //[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
 
 class Traverser
 {
-    private $unicode;
-
     private $originator;
 
-    private $wholeData;
+    private $data;
 
     private $offset;
 
-    private $remainingData;
+    private $showPreview;
 
-    public function __construct(String $wholeData, Bool $unicode = TRUE){
-        $this->unicode = $unicode;
+    private $preview;
+
+    /**
+     * @param       String                                  $data
+     * {@TODOC}
+     *
+     * @param       Bool                                    $showPreview
+     * If set to `TRUE` will preview the result of `substr($data, $offset)` in `$preview`.
+     * Enabling this has no purpose except for debugging. Keeping it enabled will slow down
+     * the operations, so it must be enabled only if needed.
+     */
+    public function __construct(String $data, Bool $showPreview = FALSE){
         $this->originator = $this;
-        $this->wholeData = $wholeData;
-        $this->remainingData = $wholeData;
+        $this->data = $data;
+        $this->showPreview = $showPreview;
+        $this->preview = NULL;
         $this->setOffset(0);
     }
 
@@ -46,7 +55,7 @@ class Traverser
             throw new Error("Branch can be imported only from a traverser with the same origin");
         }
         $this->offset = $traverser->offset;
-        $this->remainingData = $traverser->remainingData;
+        $this->preview = $traverser->preview;
     }
 
     public function createBranch(): Traverser{
@@ -57,18 +66,13 @@ class Traverser
 
     private function setOffset(Int $offset){
         $this->offset = $offset;
-        $this->remainingData = substr($this->wholeData, $this->offset);
-    }
-
-    private function advanceOffset(Int $length){
-        if($length < 0){ throw new Error(); }
-        if($length === 0){ return; }
-        $this->offset += $length;
-        $this->remainingData = substr($this->remainingData, $length);
+        if($this->showPreview){
+            $this->preview = substr($this->data, $this->offset);
+        }
     }
 
     public function isEOF(): Bool{
-        return $this->remainingData === "";
+        return $this->offset === strlen($this->data);
     }
 
     //[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
@@ -78,14 +82,14 @@ class Traverser
     }
 
     private function execRegexp(String $regexp): ?String{
-        $result = preg_match("/^(" . $regexp . ")/s" . ($this->unicode? "u" : ""), $this->remainingData, $matches);
+        $result = preg_match("/\G(" . $regexp . ")/s", $this->data, $matches, 0, $this->offset);
 
         if($result === FALSE){
             throw new Error("PCRE ERROR: " . preg_last_error());
         }
 
         if($result === 1){
-            $this->advanceOffset(strlen($matches[0]));
+            $this->setOffset($this->offset + strlen($matches[0]));
             return $matches[0];
         }
 
@@ -98,8 +102,8 @@ class Traverser
 
     public function eatStr(String $string): ?String{
         $length = strlen($string);
-        if(substr($this->remainingData, 0, $length) === $string){
-            $this->advanceOffset($length);
+        if(substr($this->data, $this->offset, $length) === $string){
+            $this->setOffset($this->offset + $length);
             return $string;
         }
         return NULL;
@@ -112,13 +116,13 @@ class Traverser
         if($length === 0){
             return "";
         }
-        $string = mb_substr($this->remainingData, 0, $length);
-        if(mb_strlen($string) === $length){
-            $this->advanceOffset(strlen($string));
-            return $string;
-        }else{
+        $remaining = substr($this->data, $this->offset);
+        $string = mb_substr($remaining, 0, $length);
+        if(mb_strlen($string) !== $length){
             return NULL;
         }
+        $this->setOffset($this->offset + strlen($string));
+        return $string;
     }
 
     public function eatAll(): String{
