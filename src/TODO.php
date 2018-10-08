9776ed6d9d1f2573cdb1dffd485c37a49dc67752
Netmosfera <netmosfera@gmail.com>
2018-10-08 05:28:58 +0200

    Update TODO list, remove entries no longer relevant.

--- src/TODO.php
+++ src/TODO.php
@@ -3,9 +3,3 @@
 // @TODO test null in all tokens
 
 // @TODO check that all functions have the return type
-
-// @TODO make eatStringToken tests less pedantic
-
-// @TODO the tests in NameTokenTest, StringTokenTest, BadStringTokenTest,
-// URLTokenTest, BadURLTokenTest and BadURLRemnantsTokenTest
-// are essentially the same: merge them in a single class
