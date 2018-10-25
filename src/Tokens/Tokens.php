035ae8b79a7171b29de085d6f85fe9f5c76433da
Netmosfera <netmosfera@gmail.com>
2018-10-25 09:19:47 +0200

    Implement Token collection (Tokens).

---
+++ src/Tokens/Tokens.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types = 1);
+
+namespace Netmosfera\PHPCSSAST\Tokens;
+
+use function Netmosfera\PHPCSSAST\match;
+
+/**
+ * A list of {@see RootToken}s.
+ */
+class Tokens
+{
+    /**
+     * @var         RootToken[]
+     * `Array<Int, RootToken>`
+     */
+    private $_tokens;
+
+    /**
+     * @param       String $text
+     * `String`
+     * @TODOC
+     */
+    public function __construct(array $tokens){
+        $this->_tokens = $tokens;
+    }
+
+    /** @inheritDoc */
+    public function __toString(): String{
+        return implode("", $this->_tokens); // @TODO memoize
+    }
+
+    /** @inheritDoc */
+    public function tokens(): array{
+        return $this->_tokens;
+    }
+
+    /** @inheritDoc */
+    public function equals($other): Bool{
+        return
+            $other instanceof self &&
+            match($this->_tokens, $other->_tokens);
+    }
+}
